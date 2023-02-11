259a6545e71c34efcf9559bd1734f7706f26d591
Kyle <xkylechan@gmail.com>
2023-02-10 19:50:22 -0700

    Able to draw on the canvas now properly

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const container = document.getElementById('container');
 
+
 function createGrid() {
   for (let i = 0; i < 16; i++) {  
     const rowGrid = document.createElement('div');
     rowGrid.classList.add('row');
     container.appendChild(rowGrid);
     for (let i = 0; i < 16; i++) {
-      const column = document.createElement('div');
-      column.setAttribute('id', 'column');
-      rowGrid.appendChild(column);
+      const columnGrid = document.createElement('div');
+      columnGrid.classList.add('column');
+      rowGrid.appendChild(columnGrid);
     }
   }
 }
 
-
 createGrid();
-// createColumns();
-console.log('hello');
+
+
+const sketch = document.querySelectorAll('.column');
+sketch.forEach(function(input){
+  input.addEventListener('mouseover', function hover() {
+    input.style.backgroundColor = 'lightblue';
+  })
+})
