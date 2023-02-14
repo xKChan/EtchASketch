60c5c219040cf20a701784dc9e287c14da64606d
Kyle <xkylechan@gmail.com>
2023-02-14 14:38:53 -0700

    Complete EtchSktch project, can draw with random RGB colors

--- script.js
+++ script.js
@@ -12,14 +12,27 @@ function createGrid() {
       rowGrid.appendChild(columnGrid);
     }
   }
+
+  const sketch = document.querySelectorAll('.column');
+  sketch.forEach(function(input){
+    input.addEventListener('mouseover', function hover() {
+      let x = Math.floor(Math.random() * 256);
+      let y = Math.floor(Math.random() * 256);
+      let z = Math.floor(Math.random() * 256);
+      input.style.backgroundColor = "rgb(" + x + "," + y + "," + z + ")";
+    })
+  })
 }
 
 createGrid();
 
+function newSketchPad(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+  createGrid();
+}
 
-const sketch = document.querySelectorAll('.column');
-sketch.forEach(function(input){
-  input.addEventListener('mouseover', function hover() {
-    input.style.backgroundColor = 'lightblue';
-  })
-})
+document.getElementById("reset").onclick = function() {
+  newSketchPad(container)
+};
